5b8636495cefc4766e49da4c09b61fff6e22c007
Sebastian Gubiec <seb.gub@wp.pl>
2022-01-28 14:21:09 +0100

    try heroku open

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
-});
-
-
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
@@ -29,4 +24,8 @@ app.get('*', (req, res) => {
 
 app.use((req, res) => {
   res.status(404).json({message: '404 not found...'});
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
