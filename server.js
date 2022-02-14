f6829d521cb507216cd21f5e2c993db257063c0d
Sebastian Gubiec <seb.gub@wp.pl>
2022-02-14 20:22:04 +0100

    mongoAtlas

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const socket = require('socket.io');
 const path = require('path');
 const mongoose = require('mongoose');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use;
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
@@ -23,10 +27,13 @@ app.use((req, res) => {
   res.status(404).json({ message: '404 not found...' });
 });
 
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  'mongodb+srv://sebgub7:<password>@cluster0.nzkum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
@@ -35,6 +42,13 @@ db.once('open', () => {
 });
 db.on('error', (err) => console.log('Error' + err));
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running');
 });
+
+const io = socket(server);
+io.on('connection', (socket) => {
+  console.log('New socket ', socket.id);
+});
+
+module.exports = server;
